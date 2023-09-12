bf5b9b5685e64aa9b1a11cb9b42b6916ba7420d6
GreenOniX <greenonix5@gmail.com>
2023-09-12 10:39:06 +0700

    Create Source2.cpp

---
+++ Source2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+// Исходный код на с++
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	cout << "Helllo world! Привет мир!" << endl
+		int var = 42;
+		int unusing;
+}

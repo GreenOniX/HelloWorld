ac86d9d7f11641647666c52ac19d0127fda559e5
GreenOniX <greenonix5@gmail.com>
2023-09-12 10:38:56 +0700

    Create C с ошибками.cpp

---
+++ Hello world/C с ошибками.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+// Исходный код на C
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	printf("Hello World! Привет мир!\n")
+		int var = 42;
+		int unusing;
+}

bc93c3e1df9d36d65b1d9fadb4cf12d6c2b67bfa
GreenOniX <greenonix5@gmail.com>
2023-09-12 10:34:23 +0700

    Update Hello world.cpp

--- Hello world/Hello world.cpp
+++ Hello world/Hello world.cpp
@@ -3,13 +3,10 @@
 
 using namespace std;
 
-/**
- * @brief Л/р 1 Знакомство со средой разработки
- * Исходный код на C++
- * @return Всегда 0
-*/
+
 int main() {
     setlocale(LC_ALL, "ru");
 
     cout << "Hello World! Привет мир!" << endl;
 }
+
